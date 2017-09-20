0018e8a24ed401ce30b575e2bf1301eef5fe0f74
Eun Leem <team88.eleem@gmail.com>
2017-09-20 12:55:55 +0900

    Remove jQuery

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,9 +1,14 @@
 import { TestClass } from './testClass';
 
-(function($) {
-  console.log("jQuery loaded.");
+(function() {
+  console.log("Module loaded.");
   let ins = new TestClass();
   ins.hello();
-  $(".box").html("Hello");
 
-})(jQuery)
+  let boxes = document.querySelectorAll('.box') as NodeListOf<HTMLDivElement>;
+
+  if (boxes.length > 0) {
+    boxes[0].innerHTML = "Hello World! Box has been intialized.";
+  }
+
+})();
